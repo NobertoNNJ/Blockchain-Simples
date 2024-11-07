852849f866da7e8ae083d7d40e6307ede97b1a13
Noberto <nobertojunior2001@gmail.com>
2024-11-07 18:33:54 -0300

    feat: incrementação da blockchain, adicionando PoW, nonce, dificuldade, validação de endereço e historico de transações do endereço.

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ class Block{
         this.timestamp = timestamp;
         this.transactions = transactions;
         this.previousHash = previousHash;
-        this.hash = this.calculatorHash();
+        this.hash = '';
         this.merkleTree = this.createMerkleTree();
+        this.nonce = 0;
+        this.difficulty = 3;
     };
 
     calculatorHash(){
-        return crypto.createHash('sha256').update(this.previousHash + this.timestamp + JSON.stringify(this.transactions)).digest('hex');
+        return crypto.createHash('sha256').update(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).digest('hex');
     }
 
     createMerkleTree(){
@@ -37,15 +39,23 @@ class Block{
         }
         return hashes[0];
     }
+    mineBlock(){
+        while(this.hash.substring(0, this.difficulty) != Array(this.difficulty + 1).join('0')){
+            this.nonce++;
+            this.hash = this.calculatorHash()
+        }
+        console.log(`Bloco minerado com o hash: ${this.hash} e com o nonce: ${this.nonce}.`);
+    }
 }
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
-        this.pendingTransactions = [];
+        this.difficulty = 3;
+        this.transactionHistory = {};
     }
 
     createGenesisBlock(){
-        return new Block(Date.now(), [], '0');
+        return new Block(Date.now(), [], '0', this.difficulty);
     }
 
     getLastestBlock(){
@@ -54,19 +64,43 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLastestBlock().hash;
-        newBlock.hash = newBlock.calculatorHash();
+        newBlock.mineBlock();
         this.chain.push(newBlock);
+        
+        newBlock.transactions.forEach(transaction => this.updateTransactionHistory(transaction));
     }
 
     createTransaction(transaction){
-        this.pendingTransactions.push(transaction);
+        if(this.isValidAddress(transaction.fromUser) && this.isValidAddress(transaction.toUser)){
+
+            let block = new Block(Date.now(), [transaction], this.getLastestBlock().hash, this.difficulty);
+            this.addBlock(block);
+            
+            console.log('\nTransação adicionada');
+        }else{
+            console.log('Transação invalida, verifique os endereços.')
+        }
     }
 
-    addPendingTransaction(){
-        let block = new Block(Date.now(), this.pendingTransactions, this.getLastestBlock().hash);
-        this.addBlock(block);
-        this.pendingTransactions = [];
-        console.log(`Block added: ${block.hash}`);
+    isValidAddress(address){
+        const regex = /^7f5[a-fA-F0-9]{7}$/;
+        return regex.test(address);
+    }
+    
+    updateTransactionHistory(transaction){
+        if(!this.transactionHistory[transaction.fromUser]){
+            this.transactionHistory[transaction.fromUser] = { sent: [], received: []};
+        }
+        if(!this.transactionHistory[transaction.toUser]){
+            this.transactionHistory[transaction.toUser] = { sent: [], received: []};
+        }
+
+        this.transactionHistory[transaction.fromUser].sent.push(transaction);
+        this.transactionHistory[transaction.toUser].received.push(transaction);
+    }
+
+    getTransactionHistory(address){
+        return this.transactionHistory[address] ||{ sent: [], received: []};
     }
 
     isChainValid(){
@@ -87,19 +121,19 @@ class Blockchain{
         return true;
     }
 }
+
 console.log('Iniciando a Blockchain...');
 let gamesCoin = new Blockchain();
 
 
-gamesCoin.createTransaction(new Transaction('Wandreus', 'Noberto', 100));
+console.log('\nAdicionando Transações');
 
+gamesCoin.createTransaction(new Transaction('7f5a1b2c3d', '7f59f8e7d6', 100));
 
-console.log('Adicionando Transações');
-gamesCoin.addPendingTransaction();
+console.log('\nAdicionando Transações');
 
-gamesCoin.createTransaction(new Transaction('Noberto', 'Wandreus', 10));
+gamesCoin.createTransaction(new Transaction('7f59f8e7d6', '7f5a1b2c3d', 10));
 
-console.log('Adicionando Transações');
-gamesCoin.addPendingTransaction();
+console.log(`\nBlockchain valida? ${gamesCoin.isChainValid()}`);
 
-console.log(`Blockchain valida? ${gamesCoin.isChainValid()}`);
+console.log("\nHistórico de transações do endereço '7f59f8e7d6':", gamesCoin.getTransactionHistory('7f59f8e7d6'));
